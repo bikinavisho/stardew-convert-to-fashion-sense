cbddaa1873b7b38bcf4fed90a03e5c44929ff1b7
bikinavisho <bikinavisho@yahoo.com>
2022-06-13 21:44:44 -0500

    feature: add shirt and hat conversion

--- README.md
+++ README.md
@@ -14,45 +14,111 @@ click "Download frames as a zip", then extract that to get all your individual
 files for your hairstyles.
 
 ## Step 2 : Get the code ready
-- Clone the project
+- Clone the project down from GitHub
 - run `npm install` inside the project directory
-- create two directories inside the project: `input` and `output` 
+- create two directories inside the project: `input` and `output`
 - code is ready to run! just a few more steps
 
 ## Step 3 - Sort the hairstyles
 Now it's time for this code to shine! Note that this step requires human sorting
 of the files into "colorable" and "non-colorable" hairstyles. Colorable
 hairstyles are usually in grayscale, whereas non-colorable are usually not in
-grayscale. If you don't want to do this, you can just have them all be colorable,
-which is the code's default.
+grayscale. If you don't want to do this, you can just have them all not be
+colorable, which is the code's default.
 
 ## Step 4 - Run the code!
 First, drag all the colorable files into the `input` directory in this project.
-Then, run `npm start`. The code will execute and your Fashion Sense converted
-hairstyles will be in the `output` directory, with the file names corresponding
-to the tile number that the program from Step 1 gave them.
+Then, run
+`npm start -- -- --conversionType=hair --isColorable --authorName=NAMEOFAUTHOR --hairDescriptor=ADJECTIVE`.
+There are a few customizable parameters that you can alter accordingly. First,
+you should enter the original name of the author whose hairstyles you are
+converting. This will enter it in Fashion Sense as `AUTHOR's Hairstyle 01`. If
+you specify a `hairDescriptor`, it will add that after the author's name,
+resulting in `AUTHOR's ADJECTIVE Hairstyle 01`, which makes it a lot easier to
+search for hairstyles you want in the Fashion Sense interface later.
+
+For example, if you enter the command `npm start -- -- --conversionType=hair --isColorable --authorName=BikiFae --hairDescriptor=Braided`,
+it will result in hairstyles named as `BikiFae's Braided Hairstyle 01 (Dyeable)`.
+
+After executing the command above with the parameters you want, you will find
+the Fashion Sense converted hairstyles in the `output` directory, with the file
+names corresponding to the tile number that the program from Step 1 gave them.
 
 Move these files to your Fashion Sense pack, to the "Hairs" directory.
 
 Delete the files in the `input` directory.
 Move the non-colorable files into the `input` directory.
-Go into the code and comment out line 16, and uncomment out line 17.
 
-### Before
+Now run
+`npm start -- -- --conversionType=hair --authorName=NAMEOFAUTHOR --hairDescriptor=ADJECTIVE`.,
+and your Fashion Sense converted hairstyles will again be found in the `output`
+directory.
+(If you're wondering why `--isColorable` is no longer there, it's because the
+code defaults to isColorable:false, and adding `--isColorable` changes it to
+true. So when you want it to not be colorable, all you have to do is remove the
+parameter.)
+
+Move these files from the `output` directory to your Fashion Sense pack, inside
+the "Hairs" directory.
+
+You're done!
+
+
+# Convert Shirts / Hats from Json Assets to Fashion Sense!
+
+New functionality has been added to the project! Now you can convert Json Assets
+clothing and hat packs into Fashion Sense content packs.
+
+## Step 1 - Copy the files over
+Inside the JSON assets package, copy the entire contents of the Hats OR Shirts
+directory inside the JA package over to the `input` directory. This application
+can only handle converting ONLY hats OR shirts at one time.
+
+## Step 2 - Run the Code!
+Run the following command:
+
+for a shirt:
 ```
-let hairstyleObject = _.cloneDeep(COLORABLE_HAIR);
-//let hairstyleObject = _.cloneDeep(UNCOLORABLE_HAIR);
+npm start -- -- --conversionType=TYPE --authorName=AUTHOR --prefix=PREFIX --hasSleeves
 ```
-
-### After
+for a hat:
 ```
-//let hairstyleObject = _.cloneDeep(COLORABLE_HAIR);
-let hairstyleObject = _.cloneDeep(UNCOLORABLE_HAIR);
+npm start -- -- --conversionType=TYPE --authorName=AUTHOR --prefix=PREFIX
 ```
+- where `TYPE` is either `hat` or `shirt`
+- where `AUTHOR` is the name of the original author who created the shirts/hats
+- where `PREFIX` is a descriptor you want to go between the author name and the
+name of the item for all items in the set, for example, if it's a summer set,
+and you pass in `--prefix=Summer` then the items will be called something like
+`AUTHOR's Summer Blouse`. This is an OPTIONAL parameter and be removed if you
+do not want to use it.
+- if you don't want any of the shirts to have sleeves in the Fashion Sense
+framework, remove the `--hasSleeves` parameter from the command above. This
+only applies if you are using `--conversionType=shirt`.
 
-Now run `npm start` again, and your Fashion Sense converted hairstyles will again
-be found in the `output` directory.
+Note: currently all sleeves default to white, since it's difficult to
+programmatically and accurately determine which color the sleeves *should* be.
+You can manually go in and change this later, file by file, if you so choose.
 
-Move these files to your Fashion Sense pack, inside the "Hairs" directory.
+## Step 3 - Copy the Files over to your Fashion Sense pack!
+You will find the Fashion Sense shirts inside the `output` directory after
+running the command mentioned above. Go ahead and drag the contents of the
+`output` directory into a directory called `Shirts` in your Fashion Sense pack.
 
-You're done! 
+
+## Step 4 (Optional) - Fix the Sleeve Colors
+1. Open the shirt.png file in your image editor of choice, one that specifically
+can read RGB values. I personally use GIMP for this, since it's free and
+open-source.
+2. Use the eye drop tool to select the color you want the sleeves to be, and
+find its RGB value.
+3. Open the shirt.json
+4. The property you'll be editing is `SleeveColors` and you'll have to edit it
+to be the same for both `FrontShirt`, `BackShirt`, `LeftShirt`, and `RightShirt`
+properties. As stated in the Fashion Sense documentation, "The first color
+given will replace the vanilla sleeve's darkest color, while the second
+replaces the medium color and the last will replace the lightest color."
+5. Take the RGB values and put them in that order for the `SleeveColors`
+property of all four options.
+6. Test it in game, and if you don't like the color, tweak it and preview it
+quickly via `fs_reload`.
--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const fsPromises = require('fs').promises;
-const editJsonFile = require("edit-json-file");
+const loadJson5File = require('read-json5-file');
 const _ = require('lodash');
 const jsonFile = require('jsonfile')
+const path = require('path');
+
+const commandLineArgs = require('command-line-args')
+const optionDefinitions = [
+   { name: 'conversionType', type: String, defaultOption: true, alias: 't' },
+   { name: 'authorName', type: String, alias: 'a' },
+   { name: 'prefix', type: String, alias: 'p' },
+   { name: 'hairDescriptor', type: String },
+   { name: 'isColorable', type: Boolean, alias: 'c' },
+   { name: 'hasSleeves', type: Boolean, alias: 's' },
+];
+const options = commandLineArgs(optionDefinitions)
+
+console.log('options: ', options);
 
 // empty out anything that previously was generated in the output folder,
 // then recreate the output folder
@@ -10,33 +24,219 @@ fsPromises.rm('./output', {recursive: true}).then(() => {
   fsPromises.mkdir('./output').then(() => {
     fsPromises.readdir('./input').then((fileNames) => {
       let numberOfFiles = fileNames.length;
+
+      // this variable only truly applies to the isHair flow
+      let isColorable = false;
+      if (options.isColorable) {
+        isColorable = options.isColorable;
+      }
+
+
+      // toggle the type of conversion you're performing today
+      let isHair = false;
+      let isShirt = false;
+      let isHat = false;
+
+      let {conversionType} = options;
+      console.log('conversionType: ', conversionType);
+      if (conversionType) {
+        conversionType = conversionType.toLowerCase();
+
+        const validConversionTypes = ['hat', 'shirt', 'hair'];
+        let isValidConversionType = !_.isEmpty(_.intersection(_.words(conversionType), validConversionTypes));
+        if (isValidConversionType) {
+          switch(conversionType) {
+            case 'hat':
+              isHat = true;
+              isShirt = false;
+              isHair = false;
+              break;
+            case 'shirt':
+              isShirt = true;
+              isHair = false;
+              isHat = false;
+              break;
+            case 'hair':
+              isHair = true;
+              isHat = false;
+              isShirt = false;
+              break;
+            default:
+              console.log('failed to specify which type of conversion to perform');
+          }
+        }
+      }
+
+
       fileNames.forEach((fileName) => {
-        const paddedIndex = _.replace(_.replace(fileName, 'tile', ''), '.png', '');
-        fsPromises.mkdir(`./output/${paddedIndex}`).then(() => {
-          let hairstyleObject = _.cloneDeep(COLORABLE_HAIR);
-          //let hairstyleObject = _.cloneDeep(UNCOLORABLE_HAIR);
+        // ===================================================================
+        //                      H A T S
+        // ===================================================================
+        if (isHat) {
+          let file = readJsonFile(fileName, 'hat.json');
+          if (!file.Name) {
+            throw new Error("json file invalid");
+          }
+          let hatObject = _.cloneDeep(HAT_TEMPLATE);
 
-          hairstyleObject["Name"] = paddedIndex;
+          hatObject["Name"] = file.Name;
+          if (options.authorName) {
+            hatObject["Name"] = `${options.authorName}'s '` + hatObject["Name"];
+          }
+          if (!options.authorName && options.prefix) {
+            hatObject["Name"] = `${options.prefix} '` + hatObject["Name"];
+          }
+          //hatObject["Name"] = "Author's " + shirtObject["Name"];
 
-          fsPromises.writeFile(`./output/${paddedIndex}/hair.json`, JSON.stringify(hairstyleObject, null, "\t")).then(() => {
-            console.log('hair.json created for ' + paddedIndex);
-          }).catch((e) => {
-            console.log('failed to create hair.json for ' + paddedIndex);
-            console.log(e);
+          if (file.ShowHair == false) {
+            hatObject["FrontHat"]["HideHair"] = true;
+            hatObject["RightHat"]["HideHair"] = true;
+            hatObject["LeftHat"]["HideHair"] = true;
+            hatObject["BackHat"]["HideHair"] = true;
+          }
+
+          fsPromises.mkdir(`./output/${fileName}/`).then(() => {
+            // write json
+            fsPromises.writeFile(`./output/${fileName}/hat.json`, JSON.stringify(hatObject, null, "\t")).then(() => {
+              console.log('hat.json created for ' + fileName);
+            }).catch((e) => {
+              console.log('failed to create hat.json for ' + fileName);
+              console.log(e);
+            });
+
+            // copy image over
+            fsPromises.copyFile(`./input/${fileName}/hat.png`, `./output/${fileName}/hat.png`).then(() => {
+              console.log('hat.png copied over for ' + fileName);
+            }).catch((e) => {
+              console.log('failed to copy hat.png for ' + fileName);
+              console.log(e);
+            });
+          }).catch((exception) => {
+            console.log(`failed to create directory for ${fileName}`);
           });
+        }
 
-          fsPromises.copyFile(`./input/${fileName}`, `./output/${paddedIndex}/hair.png`).then(() => {
-            console.log('hair.png copied over for ' + paddedIndex);
-          }).catch((e) => {
-            console.log('failed to copy hair.png for ' + paddedIndex);
-            console.log(e);
+        // ===================================================================
+        //                    S H I R T S
+        // ===================================================================
+        if (isShirt) {
+          let file = readJsonFile(fileName, 'shirt.json');
+          if (!file.Name) {
+            throw new Error(`json file "${fileName}/shirt.json" invalid`);
+          }
+          isColorable = Boolean(file.Dyeable);
+          let shirtObject = _.cloneDeep(isColorable ? COLORABLE_SHIRT : NON_COLORABLE_SHIRT);
+
+
+          shirtObject["Name"] = file.Name;
+          if (options.authorName) {
+            shirtObject["Name"] = `${options.authorName}'s ${options.hasSleeves ? '' : 'Summer '}Shirt ` + shirtObject["Name"];
+          }
+          if (!options.authorName && options.prefix) {
+            shirtObject["Name"] = `${options.prefix} '` + shirtObject["Name"];
+          }
+          //shirtObject["Name"] = "Author's " + shirtObject["Name"];
+          if  (isColorable) {
+            shirtObject["Name"] += " (Dyeable)";
+          }
+
+          const shirtModelArray = ['BackShirt', 'FrontShirt', 'LeftShirt', 'RightShirt'];
+
+          if (options.hasSleeves == false) {
+            shirtModelArray.forEach((shirtModel) => {
+              delete shirtObject[shirtModel].SleeveColors;
+            })
+          }
+
+          fsPromises.mkdir(`./output/${fileName}/`).then(() => {
+            // --write json--
+            fsPromises.writeFile(`./output/${fileName}/shirt.json`, JSON.stringify(shirtObject, null, "\t")).then(() => {
+              console.log('shirt.json created for ' + fileName);
+            }).catch((e) => {
+              console.log('failed to create shirt.json for ' + fileName);
+              console.log(e);
+            });
+
+            // --copy image over--
+            // default to female version because most don't have a difference
+            // and I usually play as a female character ;P
+            if (file.HasFemaleVariant) {
+              fsPromises.copyFile(`./input/${fileName}/female.png`, `./output/${fileName}/shirt.png`).then(() => {
+                console.log('female.png copied over for ' + fileName);
+              }).catch((e) => {
+                console.log('failed to copy female.png for ' + fileName);
+                console.log(e);
+              });
+            } else {
+              fsPromises.copyFile(`./input/${fileName}/male.png`, `./output/${fileName}/shirt.png`).then(() => {
+                console.log('male.png copied over for ' + fileName);
+              }).catch((e) => {
+                console.log('failed to copy male.png for ' + fileName);
+                console.log(e);
+              });
+            }
+          }).catch((exception) => {
+            console.log(`failed to create directory for ${fileName}`);
           });
-        });
+        }
+        // ===================================================================
+        //                              H A I R
+        // ===================================================================
+        if (isHair) {
+          // expected input from the sprite cutter website referenced in the README
+          // in the format `tile000.png`...
+          const paddedIndex = _.replace(_.replace(fileName, 'tile', ''), '.png', '');
+          fsPromises.mkdir(`./output/${paddedIndex}`).then(() => {
+            if (isHair) {
+              let hairstyleObject = _.cloneDeep(isColorable ? COLORABLE_HAIR : UNCOLORABLE_HAIR);
+
+              //NOTE: CHANGE THE BELOW VARIABLES TO MATCH YOUR RUN
+              let authorName = '';
+              let hairDescriptor = 'Long';    // Long, Short, Wavy, etc.
+
+              if (options.authorName) {
+                authorName = options.authorName;
+              }
+              if (options.hairDescriptor) {
+                hairDescriptor = options.hairDescriptor;
+              }
+
+              // set the object's display name
+              hairstyleObject["Name"] = `${authorName}'s ${hairDescriptor} Hairstyle ${paddedIndex}`;
+              //hairstyleObject["Name"] = `${hairDescriptor} Sakura Hairstyle ${paddedIndex}`;
+              if  (isColorable) {
+                hairstyleObject["Name"] += " (Dyeable)";
+              }
+
+              // write to the json
+              fsPromises.writeFile(`./output/${paddedIndex}/hair.json`, JSON.stringify(hairstyleObject, null, "\t")).then(() => {
+                console.log('hair.json created for ' + paddedIndex);
+              }).catch((e) => {
+                console.log('failed to create hair.json for ' + paddedIndex);
+                console.log(e);
+              });
+
+              // copy the image file over
+              fsPromises.copyFile(`./input/${fileName}`, `./output/${paddedIndex}/hair.png`).then(() => {
+                console.log('hair.png copied over for ' + paddedIndex);
+              }).catch((e) => {
+                console.log('failed to copy hair.png for ' + paddedIndex);
+                console.log(e);
+              });
+            }
+          });
+        }
       });
     });
   });
 });
 
+
+function readJsonFile(fileName, targetJson) {
+  const fixture = path.join(__dirname, `/input/${fileName}/${targetJson}`)
+  const fileContents = loadJson5File.sync(fixture);
+  return fileContents;
+}
 
 const UNCOLORABLE_HAIR = {
   "Name": "",
@@ -161,4 +361,231 @@ const COLORABLE_HAIR = {
       "Length": 32
     }
   }
+};
+
+const COLORABLE_SHIRT = {
+  "Name": "",
+  "FrontShirt": {
+    "SleeveColors": [
+      [ 160, 160, 160 ],
+      [ 201, 201, 201 ],
+      [ 237, 237, 237 ],
+    ],
+    "StartingPosition": {
+      "X": 0,
+      "Y": 0
+    },
+    "BodyPosition": {
+      "X": 0,
+      "Y": 0
+    },
+    "ShirtSize": {
+      "Width": 8,
+      "Length": 8
+    }
+  },
+  "RightShirt": {
+    "SleeveColors": [
+      [ 160, 160, 160 ],
+      [ 201, 201, 201 ],
+      [ 237, 237, 237 ],
+    ],
+    "StartingPosition": {
+      "X": 0,
+      "Y": 8
+    },
+    "BodyPosition": {
+      "X": 0,
+      "Y": 0
+    },
+    "ShirtSize": {
+      "Width": 8,
+      "Length": 8
+    }
+  },
+  "LeftShirt": {
+    "SleeveColors": [
+      [ 160, 160, 160 ],
+      [ 201, 201, 201 ],
+      [ 237, 237, 237 ],
+    ],
+    "StartingPosition": {
+      "X": 0,
+      "Y": 16
+    },
+    "BodyPosition": {
+      "X": 0,
+      "Y": 0
+    },
+    "ShirtSize": {
+      "Width": 8,
+      "Length": 8
+    }
+  },
+  "BackShirt": {
+    "SleeveColors": [
+      [ 160, 160, 160 ],
+      [ 201, 201, 201 ],
+      [ 237, 237, 237 ],
+    ],
+    "StartingPosition": {
+      "X": 0,
+      "Y": 24
+    },
+    "BodyPosition": {
+      "X": 0,
+      "Y": 0
+    },
+    "ShirtSize": {
+      "Width": 8,
+      "Length": 8
+    }
+  }
+};
+const NON_COLORABLE_SHIRT = {
+  "Name": "",
+  "FrontShirt": {
+    "SleeveColors": [
+      [ 225, 218, 218],
+      [ 241, 234, 234 ],
+      [ 250, 243, 243],
+    ],
+    "DisableGrayscale": true,
+    "StartingPosition": {
+      "X": 0,
+      "Y": 0
+    },
+    "BodyPosition": {
+      "X": 0,
+      "Y": 0
+    },
+    "ShirtSize": {
+      "Width": 8,
+      "Length": 8
+    }
+  },
+  "RightShirt": {
+    "SleeveColors": [
+      [ 225, 218, 218],
+      [ 241, 234, 234 ],
+      [ 250, 243, 243],
+    ],
+    "DisableGrayscale": true,
+    "StartingPosition": {
+      "X": 0,
+      "Y": 8
+    },
+    "BodyPosition": {
+      "X": 0,
+      "Y": 0
+    },
+    "ShirtSize": {
+      "Width": 8,
+      "Length": 8
+    }
+  },
+  "LeftShirt": {
+    "SleeveColors": [
+      [ 225, 218, 218],
+      [ 241, 234, 234 ],
+      [ 250, 243, 243],
+    ],
+    "DisableGrayscale": true,
+    "StartingPosition": {
+      "X": 0,
+      "Y": 16
+    },
+    "BodyPosition": {
+      "X": 0,
+      "Y": 0
+    },
+    "ShirtSize": {
+      "Width": 8,
+      "Length": 8
+    }
+  },
+  "BackShirt": {
+    "SleeveColors": [
+      [ 225, 218, 218],
+      [ 241, 234, 234 ],
+      [ 250, 243, 243],
+    ],
+    "DisableGrayscale": true,
+    "StartingPosition": {
+      "X": 0,
+      "Y": 24
+    },
+    "BodyPosition": {
+      "X": 0,
+      "Y": 0
+    },
+    "ShirtSize": {
+      "Width": 8,
+      "Length": 8
+    }
+  }
+};
+
+const HAT_TEMPLATE = {
+	"Name": "",
+  "FrontHat": {
+    "DisableGrayscale": true,
+		"StartingPosition": {
+			"X": 0,
+			"Y": 0
+		},
+		"HeadPosition": {
+			"X": 0,
+			"Y": 0
+		},
+		"HatSize": {
+			"Width": 20,
+			"Length": 20
+		}
+	},
+	"RightHat": {
+    "DisableGrayscale": true,
+		"StartingPosition": {
+			"X": 0,
+			"Y": 20
+		},
+		"HeadPosition": {
+			"X": 0,
+			"Y": 0
+		},
+		"HatSize": {
+			"Width": 20,
+			"Length": 20
+		}
+	},
+	"LeftHat": {
+    "DisableGrayscale": true,
+		"StartingPosition": {
+			"X": 0,
+			"Y": 40
+		},
+		"HeadPosition": {
+			"X": 0,
+			"Y": 0
+		},
+		"HatSize": {
+			"Width": 20,
+			"Length": 20
+		}
+	},
+	"BackHat": {
+    "DisableGrayscale": true,
+		"StartingPosition": {
+			"X": 0,
+			"Y": 60
+		},
+		"HeadPosition": {
+			"X": 0,
+			"Y": 0
+		},
+		"HatSize": {
+			"Width": 20,
+			"Length": 20
+		}
+	}
 };
